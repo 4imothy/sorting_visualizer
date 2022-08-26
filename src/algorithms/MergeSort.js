4b859ede5cbb379436e30d2a8d12b8970ad97092
Timothy Cronin <tcron@Timothys-MacBook-Pro-2.local>
2022-08-26 11:01:14 -0400

    progress on merge

--- src/algorithms/MergeSort.js
+++ src/algorithms/MergeSort.js
@@ -10,13 +10,15 @@ const normalColor = "#34cceb";
 let visArr;
 let animSpeed;
 let useArr = [];
+let timeInc = 0;
 
 export function MergeSort(valArr, visArrP, animSpeedP) {
     visArr = visArrP;
     animSpeed = animSpeedP;
 
-    for(let i = 0; i < valArr.length; i++){
-        useArr.push({val: valArr[i],
+    for (let i = 0; i < valArr.length; i++) {
+        useArr.push({
+            val: valArr[i],
             origInd: i
         });
         console.log(useArr[i].val);
@@ -36,20 +38,58 @@ function split(arr) {
     return merge(split(left), split(arr));
 }
 
-function merge(left, right) {    
+function merge(left, right) {
+    // this arr becomes merged again and again till length is original length
     let arr = [];
 
     while (left.length && right.length) {
-        if (left[0].val < right[0].val)
+        let leftBlockStyle = visArr[left[0].origInd].style;
+        let rightBlockStyle = visArr[right[0].origInd].style;
+
+        setTimeout(() => {
+            leftBlockStyle.backgroundColor = comparingColor;
+            rightBlockStyle.backgroundColor = comparingColor;
+        }, timeInc * animSpeed);
+        timeInc++;
+        /*
+        let rightH = rightBlockStyle.height;
+        rightBlockStyle.height = leftBlockStyle.height;
+        leftBlockStyle.height = rightH;
+        */
+
+        /**
+         * arr.push(i) moves i to the end or arr. So grab the origIndex of the last element of 
+         * arr and swap that with the current.
+         * swap this field below
+         * visArr[arr[arr.length - 1].origInd + 1].height
+         */
+        if (left[0].val < right[0].val) {
+            setTimeout(() => {
+                leftBlockStyle.backgroundColor = movingColor;
+            }, timeInc * animSpeed);
+            timeInc++;
+
             arr.push(left.shift());
-        else
+        } else {
+            setTimeout(() => {
+                rightBlockStyle.backgroundColor = movingColor;
+            }, timeInc * animSpeed);
+            timeInc++;
             arr.push(right.shift());
+        }
     }
 
-    const output = [...arr, ...left, ...right];
+    //adding onto the new array, so get the last orig index in arr, go to the next one, swap those heights
+    while (left.length) {
+        arr.push(left.shift());
+    }
 
-    for(let i =0 ; i < output.length; i++)
-        console.log(output[i].val);
-    
-    return output;
+    while (right.length) {
+        arr.push(right.shift());
+    }
+
+    for (let i = 0; i < arr.length; i++)
+        console.log(arr[i].val);
+
+    return arr;
 }
